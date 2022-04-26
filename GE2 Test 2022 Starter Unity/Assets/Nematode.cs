ddf071c09e8f6bbf7911df0ea646a07c389be114
Dongjieyang <56842526+Dongjieyang@users.noreply.github.com>
2022-04-26 12:00:30 +0100

    bla

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -5,12 +5,44 @@ using UnityEngine;
 public class Nematode : MonoBehaviour
 {
     public int length = 5;
+    public Vector3 sizeSegment;
+    public int nematodeDensity;
 
     public Material material;
 
+    public GameObject spheresPrefab;
+
+    public Vector3 firstSegment;
+    private int rotate_x;
+    private int rotate_y;
+    private int rotate_z;
     void Awake()
     {
-        // Put your code here!
+        for( int i =0;i<length;i++)
+        {
+            firstSegment.x +=1.1f;
+
+            rotate_x += Random.Range(10,20);
+        
+            rotate_y += Random.Range(10, 20);
+
+            rotate_z += Random.Range(10, 20);
+            if (i <= length / 2)
+            {
+                sizeSegment.x += 0.2f;
+                sizeSegment.y += 0.2f;
+                sizeSegment.z += 0.2f;
+            }
+            if(i>=length/2)
+            {
+                sizeSegment.x -= 0.2f;
+                sizeSegment.y -= 0.2f;
+                sizeSegment.z -= 0.2f;
+            }
+            spheresPrefab.transform.localScale = sizeSegment;
+
+            GameObject nematode = Instantiate(spheresPrefab,firstSegment,Quaternion.Euler(rotate_x,rotate_y,rotate_z));
+        }
     }
 
 
--- GE2 Test 2022 Starter Unity/Assets/NematodeSchool.cs
+++ GE2 Test 2022 Starter Unity/Assets/NematodeSchool.cs
@@ -10,11 +10,17 @@ public class NematodeSchool : MonoBehaviour
     public int radius = 50;
     
     public int count = 10;
-
+    public Vector3 firstNematode;
     // Start is called before the first frame update
     void Awake()
     {
-        // Put your code here
+        for(int i=0;i<count;i++)
+        {
+            firstNematode.x = Random.Range(0,radius);
+            firstNematode.y = Random.Range(0, radius);
+            firstNematode.z = Random.Range(0, radius);
+            GameObject nematodes = Instantiate(prefab ,firstNematode , Quaternion.Euler(0, 0, 0));
+        }
     }
 
     // Update is called once per frame
